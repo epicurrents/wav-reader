793eee49aeae4fa9fa0ab232401c7f7bd4da90fa
sam-19 <26563023+sam-19@users.noreply.github.com>
2026-05-29 12:48:29 +0200

    Don't type-check during webpack build
    Signed-off-by: sam-19 <26563023+sam-19@users.noreply.github.com>

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,14 @@ module.exports = {
         rules: [
             {
                 test: /\.tsx?$/,
-                use: 'ts-loader',
+                use: {
+                    loader: 'ts-loader',
+                    options: {
+                        // Suppress declaration-file emit during the webpack pass.
+                        // Full type-checking and .d.ts generation are handled by build:tsc.
+                        transpileOnly: true,
+                    },
+                },
                 exclude: '/node_modules/',
             },
         ],
@@ -40,6 +47,6 @@ module.exports = {
             '#types': path.resolve(__dirname, 'src', 'types'),
             '#workers': path.resolve(__dirname, 'src', 'workers'),
         },
-        symlinks: false
+        symlinks: true
     },
 }
